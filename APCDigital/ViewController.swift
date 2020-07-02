9bc8ac21f7bb613f22370c5b90a538a09acd8f4a
shi-n <shi-n_xyz@cloudsquare.jp>
2020-07-03 06:45:38 +0900

    Initial Commit

---
+++ APCDigital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APCDigital
+//
+//  Created by Shin Inaba on 2020/07/03.
+//  Copyright © 2020 shi-n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
