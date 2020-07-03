5acd85baf4ae3e21718b8bb4b76e02d3af8bddc8
shi-n <shi-n_xyz@cloudsquare.jp>
2020-07-04 08:17:58 +0900

    Page Information Display

--- APCDigital/ViewController.swift
+++ APCDigital/ViewController.swift
@@ -15,9 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var month: UILabel!
     @IBOutlet weak var day1: UILabel!
     @IBOutlet weak var day2: UILabel!
+    @IBOutlet weak var day3: UILabel!
+    @IBOutlet weak var day4: UILabel!
+    @IBOutlet weak var day5: UILabel!
+    @IBOutlet weak var day6: UILabel!
+    @IBOutlet weak var day7: UILabel!
+    @IBOutlet weak var fromDay: UILabel!
+    @IBOutlet weak var toDay: UILabel!
+    @IBOutlet weak var weekOfYear: UILabel!
     
-    var week = 1
-    var dateMonday = Date()
+    var pageMonday = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +43,17 @@ class ViewController: UIViewController {
         pKCanvasView.addGestureRecognizer(swipeRight)
 
         print(Date().description(with: Calendar.current.locale))
+        
+        let weekday = Calendar.current.component(.weekday, from: Date())
 
-        let matching = DateComponents(weekday: 2)
-        dateMonday = Calendar.current.nextDate(after: dateMonday, matching: matching, matchingPolicy: .nextTime, direction: .backward)!
-        let monday = Calendar.current.dateComponents(in: .current, from: dateMonday)
-        print(monday)
-        self.day1.text = String(monday.day!)
-        self.month.text = String(monday.month!)
+        if weekday != 2 {
+            let matching = DateComponents(weekday: 2)
+            pageMonday = Calendar.current.nextDate(after: pageMonday, matching: matching, matchingPolicy: .nextTime, direction: .backward)!
+        }
+        else {
+            pageMonday = Date()
+        }
+        updateDays()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -59,26 +70,38 @@ class ViewController: UIViewController {
     }
 
     @objc func swipeLeft(sender: UISwipeGestureRecognizer) {
-        week = week + 1
-        print(week)
-
         let matching = DateComponents(weekday: 2)
-        dateMonday = Calendar.current.nextDate(after: dateMonday, matching: matching, matchingPolicy: .nextTime, direction: .forward)!
-        let monday = Calendar.current.dateComponents(in: .current, from: dateMonday)
-        print(monday)
-        self.day1.text = String(monday.day!)
-        self.month.text = String(monday.month!)
+        pageMonday = Calendar.current.nextDate(after: pageMonday, matching: matching, matchingPolicy: .nextTime, direction: .forward)!
+        updateDays()
     }
 
     @objc func swipeRight(sender: UISwipeGestureRecognizer) {
-        week = week - 1
-        print(week)
         let matching = DateComponents(weekday: 2)
-        dateMonday = Calendar.current.nextDate(after: dateMonday, matching: matching, matchingPolicy: .nextTime, direction: .backward)!
-        let monday = Calendar.current.dateComponents(in: .current, from: dateMonday)
+        pageMonday = Calendar.current.nextDate(after: pageMonday, matching: matching, matchingPolicy: .nextTime, direction: .backward)!
+        updateDays()
+    }
+    
+    func updateDays() {
+        let monday = Calendar.current.dateComponents(in: .current, from: pageMonday)
+        let tuesday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 1))
+        let wednesday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 2))
+        let thursday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 3))
+        let friday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 4))
+        let saturday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 5))
+        let sunday = Calendar.current.dateComponents(in: .current, from: pageMonday + (86400 * 6))
         print(monday)
         self.day1.text = String(monday.day!)
+        self.day2.text = String(tuesday.day!)
+        self.day3.text = String(wednesday.day!)
+        self.day4.text = String(thursday.day!)
+        self.day5.text = String(friday.day!)
+        self.day6.text = String(saturday.day!)
+        self.day7.text = String(sunday.day!)
         self.month.text = String(monday.month!)
+        self.fromDay.text = Calendar.current.shortStandaloneMonthSymbols[monday.month! - 1] + " " + String(monday.day!)
+        self.toDay.text = "to " + Calendar.current.shortStandaloneMonthSymbols[sunday.month! - 1] + " " + String(sunday.day!)
+        self.weekOfYear.text = String(Calendar.current.component(.weekOfYear, from: pageMonday)) + " week"
     }
+    
 }
 
