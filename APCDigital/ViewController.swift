d9e4dd655839bb787e4e1159daacd2621568ac30
shi-n <shi-n_xyz@cloudsquare.jp>
2020-07-03 20:35:30 +0900

    New Create Project Start

--- APCDigital/ViewController.swift
+++ APCDigital/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import PencilKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pKCanvasView: PKCanvasView!
+    @IBOutlet weak var day1: UILabel!
+    
+    var week = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        pKCanvasView.allowsFingerDrawing = false
+        pKCanvasView.isOpaque = false
+        pKCanvasView.backgroundColor = .clear
+        pKCanvasView.overrideUserInterfaceStyle = .light
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeLeft(sender:)))
+        swipeLeft.direction = .left
+        pKCanvasView.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeRight(sender:)))
+        swipeRight .direction = .right
+        pKCanvasView.addGestureRecognizer(swipeRight)
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if let window = self.pKCanvasView.window {
+            let toolPicker = PKToolPicker.shared(for: window)
+            toolPicker?.addObserver(pKCanvasView)
+            toolPicker?.setVisible(true, forFirstResponder: pKCanvasView)
+            toolPicker?.overrideUserInterfaceStyle = .light
+            pKCanvasView.becomeFirstResponder()
+            print("PKToolPicker Set")
+        }
     }
 
+    @objc func swipeLeft(sender: UISwipeGestureRecognizer) {
+        week = week + 1
+        print(week)
+    }
 
+    @objc func swipeRight(sender: UISwipeGestureRecognizer) {
+        week = week - 1
+        print(week)
+    }
 }
 
