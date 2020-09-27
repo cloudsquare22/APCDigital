a609d3f6d8bab9d832a909d5216cca3e792313fa
shi-n <shi-n_xyz@cloudsquare.jp>
2020-09-27 10:37:57 +0900

    Update Logger Extension

--- APCDigital/LoggerExtension.swift
+++ APCDigital/LoggerExtension.swift
@@ -16,11 +16,31 @@ extension Logger {
         self.logLevel = Logger.level
     }
     
-    func info(_ message: String = "" , function: String = #function, line: Int = #line) {
-        self.info(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    func trace(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.trace(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
     }
 
     func debug(_ message: String = "" , function: String = #function, line: Int = #line) {
         self.debug(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    }
+
+    func info(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.info(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    }
+
+    func notice(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.notice(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    }
+
+    func warning(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.warning(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    }
+
+    func error(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.error(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
+    }
+
+    func critical(_ message: String = "" , function: String = #function, line: Int = #line) {
+        self.critical(Logger.Message(stringLiteral: String("[\(function):\(line)] \(message)")))
     }
 }
