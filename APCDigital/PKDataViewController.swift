1ba53e292a3fff2c49efab2a3047fb58ed4524d3
shi-n <shi-n_xyz@cloudsquare.jp>
2021-04-28 05:44:51 +0900

    add PKDataView select row.

--- APCDigital/PKDataViewController.swift
+++ APCDigital/PKDataViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Logging
 
 class PKDataViewController: UITableViewController {
     weak var viewController: ViewController? = nil
 
     var pages: [(year: Int , week: Int)] = []
+
+    let logger = Logger()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +53,13 @@ class PKDataViewController: UITableViewController {
             self.viewController?.updateDays()
         }
         self.tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        logger.info("Index:\(indexPath.row):\(self.pages[indexPath.row])")
+        let dataComponents = DateComponents(weekOfYear: self.pages[indexPath.row].week, yearForWeekOfYear: self.pages[indexPath.row].year)
+        let date = Calendar.current.date(from: dataComponents) // 日曜日の0:00
+        logger.info("Date:\(date?.description)")
     }
 
 }
