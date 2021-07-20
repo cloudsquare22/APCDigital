c1d063e87a2e32ad58f96179f2a0bc6131f9d6f3
shi-n <shi-n_xyz@cloudsquare.jp>
2021-07-20 22:01:45 +0900

    画面取得

---
+++ APCDigital/Device.swift
@@ -0,0 +1,16 @@
+//
+//  Device.swift
+//  APCDigital
+//
+//  Created by Shin Inaba on 2021/07/20.
+//  Copyright © 2021 shi-n. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Device {
+    static func getDevie() {
+        print("Device:\(UIScreen.main.bounds.size)")
+    }
+}
