80b9e358c48f4a6f10b159ed4b28dc501823ac98
shi-n <shi-n_xyz@cloudsquare.jp>
2022-11-03 08:37:18 +0900

    イベント追加共通化、通常表示側、イベント週から日にレベル合わせ、全日イベント対応

--- APCDigital/CalendarView.swift
+++ APCDigital/CalendarView.swift
@@ -30,9 +30,31 @@ class CalendarView: UIView {
         // 1週間分のイベントリストを日毎に展開
         var dayOfEKEventList: [[EKEvent]] = .init(repeating: [], count: 7)
         for event in eKEventList {
-            var startDateComponents = Calendar.current.dateComponents(in: .current, from: event.startDate)
-            dayOfEKEventList[startDateComponents.weekendStartMonday - 1].append(event)
+            if event.isAllDay == true {
+                var startDate = event.startDate! < base.pageMonday ? base.pageMonday : event.startDate!
+                let endDate = event.endDate!
+                while startDate <= endDate {
+                    let startDateComponents = Calendar.current.dateComponents(in: .current, from: startDate)
+                    print(startDateComponents.weekday!)
+                    let copyEvent = event.copy() as! EKEvent
+                    copyEvent.startDate = startDate
+                    dayOfEKEventList[startDateComponents.weekendStartMonday - 1].append(copyEvent)
+                    if startDateComponents.weekday! == 1 {
+                        break
+                    }
+                    startDate = startDate + TimeInterval(24 * 60 * 60)
+                }
+            }
+            else {
+                let startDateComponents = Calendar.current.dateComponents(in: .current, from: event.startDate)
+                dayOfEKEventList[startDateComponents.weekendStartMonday - 1].append(event)
+            }
         }
+        
+        // イベント追加
+//        for index in 0..<7 {
+//            APCDCalendarUtil.instance.addEvent(eKEventList: dayOfEKEventList[index], view: self)
+//        }
         
         for event in eKEventList {
             if event.calendar.title == APCDData.instance.nationalHoliday {
